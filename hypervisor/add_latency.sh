cd05b530700c0d12f2d9642d82b376eaf56a5c5f
Yuri Obshansky <yobshans@redhat.com>
2019-02-04 16:47:35 -0500

    Add files via upload

---
+++ hypervisor/add_latency.sh
@@ -0,0 +1,36 @@
+## SPINE-LEAF add latency
+
+sudo tc qdisc add dev dcn1-data root netem delay 200ms limit 15000000
+sudo modprobe ifb numifbs=4
+sudo ip link set dev ifb0 up
+sudo tc qdisc add dev dcn1-data ingress
+sudo tc filter add dev dcn1-data parent ffff: protocol ip u32 match u32 0 0 flowid 1:1 action mirred egress redirect dev ifb0
+sudo tc qdisc add dev ifb0 root netem delay 200ms limit 15000000
+
+sudo tc qdisc add dev dcn2-data root netem delay 200ms limit 15000000
+sudo ip link set dev ifb1 up
+sudo tc qdisc add dev dcn2-data ingress
+sudo tc filter add dev dcn2-data parent ffff: protocol ip u32 match u32 0 0 flowid 1:1 action mirred egress redirect dev ifb1
+sudo tc qdisc add dev ifb1 root netem delay 200ms limit 15000000
+
+sudo tc qdisc add dev dcn1-management root netem delay 200ms limit 15000000
+sudo ip link set dev ifb2 up
+sudo tc qdisc add dev dcn1-management ingress
+sudo tc filter add dev dcn1-management parent ffff: protocol ip u32 match u32 0 0 flowid 1:1 action mirred egress redirect dev ifb2
+sudo tc qdisc add dev ifb2 root netem delay 200ms limit 15000000
+
+sudo tc qdisc add dev dcn2-management root netem delay 200ms limit 15000000
+sudo ip link set dev ifb3 up
+sudo tc qdisc add dev dcn2-management ingress
+sudo tc filter add dev dcn2-management parent ffff: protocol ip u32 match u32 0 0 flowid 1:1 action mirred egress redirect dev ifb3
+sudo tc qdisc add dev ifb3 root netem delay 200ms limit 15000000
+
+sudo tc -s qdisc show dev dcn1-data
+sudo tc -s qdisc show dev ifb0
+sudo tc -s qdisc show dev dcn2-data
+sudo tc -s qdisc show dev ifb1
+sudo tc -s qdisc show dev dcn1-management
+sudo tc -s qdisc show dev ifb2
+sudo tc -s qdisc show dev dcn2-management
+sudo tc -s qdisc show dev ifb3
+
---
+++ hypervisor/remove_latency.sh
@@ -0,0 +1,32 @@
+## SPINE-LEAF remove latency
+
+sudo tc qdisc del dev ifb0 root netem delay 200ms limit 15000000
+sudo tc qdisc del dev dcn1-data ingress
+sudo ip link set dev ifb0 down
+sudo tc qdisc del dev dcn1-data root netem delay 200ms limit 15000000
+
+sudo tc qdisc del dev ifb1 root netem delay 200ms limit 15000000
+sudo tc qdisc del dev dcn2-data ingress
+sudo ip link set dev ifb1 down
+sudo tc qdisc del dev dcn2-data root netem delay 200ms limit 15000000
+
+sudo tc qdisc del dev ifb2 root netem delay 200ms limit 15000000
+sudo tc qdisc del dev dcn1-management ingress
+sudo ip link set dev ifb2 down
+sudo tc qdisc del dev dcn1-management root netem delay 200ms limit 15000000
+
+sudo tc qdisc del dev ifb3 root netem delay 200ms limit 15000000
+sudo tc qdisc del dev dcn2-management ingress
+sudo ip link set dev ifb3 down
+sudo modprobe -r ifb
+sudo tc qdisc del dev dcn2-management root netem delay 200ms limit 15000000
+
+sudo tc -s qdisc show dev dcn1-data
+sudo tc -s qdisc show dev ifb0
+sudo tc -s qdisc show dev dcn2-data
+sudo tc -s qdisc show dev ifb1
+sudo tc -s qdisc show dev dcn1-management
+sudo tc -s qdisc show dev ifb2
+sudo tc -s qdisc show dev dcn2-management
+sudo tc -s qdisc show dev ifb3
+
